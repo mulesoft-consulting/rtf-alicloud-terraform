40bc13ac996c2b391a87d25ace2b03fd52c94074
Souf <soufboulealf@gmail.com>
2020-09-01 21:15:02 +0200

    fixes issue on global ouputs

--- outputs.tf
+++ outputs.tf
@@ -8,17 +8,17 @@ output "proxy_public_ip" {
 
 
 output "controller_private_ips" {
-  value = module.rtf_cluster[0].controller_private_ips
+  value = module.rtf_cluster.controller_private_ips
 }
 
 output "worker_private_ips" {
-  value = module.rtf_cluster[0].controller_private_ips
+  value = module.rtf_cluster.controller_private_ips
 }
 
 output "controller_public_ips" {
-  value = module.rtf_cluster[0].controller_private_ips
+  value = module.rtf_cluster.controller_private_ips
 }
 
 output "worker_public_ips" {
-  value = module.rtf_cluster[0].controller_private_ips
+  value = module.rtf_cluster.controller_private_ips
 }
