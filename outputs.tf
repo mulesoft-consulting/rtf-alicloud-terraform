2b41859b1ca439ce39e149f0575dc7f897197cf2
Souf <soufboulealf@gmail.com>
2020-09-01 21:05:29 +0200

    adds output

---
+++ outputs.tf
@@ -0,0 +1,24 @@
+output "proxy_private_ip" {
+  value = length(module.rtf_cluster_proxy) > 0 ? module.rtf_cluster_proxy[0].proxy_private_ip : ""
+}
+
+output "proxy_public_ip" {
+  value = length(module.rtf_cluster_proxy) > 0 ? module.rtf_cluster_proxy[0].proxy_public_ip : ""
+}
+
+
+output "controller_private_ips" {
+  value = module.rtf_cluster[0].controller_private_ips
+}
+
+output "worker_private_ips" {
+  value = module.rtf_cluster[0].controller_private_ips
+}
+
+output "controller_public_ips" {
+  value = module.rtf_cluster[0].controller_private_ips
+}
+
+output "worker_public_ips" {
+  value = module.rtf_cluster[0].controller_private_ips
+}
