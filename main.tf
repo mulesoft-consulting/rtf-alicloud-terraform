7c18dd7fad483b4b4e4c940c27e636881714855e
Souf <soufboulealf@gmail.com>
2020-09-01 17:24:30 +0200

    injects variables to main modules

--- main.tf
+++ main.tf
@@ -1,6 +1,19 @@
 module "rtf_cluster" {
-  source = "./cluster-formation"
-  #name = "rtf-cluster"
+  source                = "./cluster-formation"
+  #Parameters
+  key_pair              = var.key_pair
+  public_key            = var.public_key
+  controllers           = var.controllers
+  workers               = var.workers
+  installer_scripts_url = var.installer_scripts_url
+  vpc_cidr              = var.cluster_vpc_cidr
+  activation_data       = var.activation_data
+  anypoint_region       = var.anypoint_region
+  anypoint_endpoint     = var.anypoint_endpoint
+  mule_license          = var.mule_license
+  enable_public_ips     = var.enable_public_ips
+  http_proxy            = var.http_proxy
+  no_proxy              = var.no_proxy
   
   providers = {
     alicloud = alicloud.cluster
@@ -9,7 +22,12 @@ module "rtf_cluster" {
 
 module "rtf_cluster_proxy" {
   source = "./cluster-proxy-formation"
-  #name   = "rtf-cluster-proxy"
+  #Parameters
+  key_pair          = var.key_pair
+  public_key        = var.public_key
+  enable_public_ips = var.enable_public_ips
+  vpc_cidr          = var.cluster_proxy_vpc_cidr
+  cluster_vpc_cidr  = var.cluster_vpc_cidr
 
   providers = {
     alicloud = alicloud.cluster_proxy
--- providers.tf
+++ providers.tf
@@ -1,15 +1,3 @@
-variable cluster_region {
-  type        = string
-  default     = ""
-  description = "the cluster's region"
-}
-
-variable cluster_proxy_region {
-  type        = string
-  default     = "eu-central-1"
-  description = "the cluster proxy's region"
-}
-
 provider "alicloud" {
   alias  = "cluster"
   region = var.cluster_region
