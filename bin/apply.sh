096b59b77b760c87c7d9f12a924f94726b3e85f0
Souf <soufboulealf@gmail.com>
2020-09-01 16:32:28 +0200

    externalizes the parameter file path

--- bin/apply.sh
+++ bin/apply.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+
+if [ "$#" -lt 1 ]; then 
+    echo "[ERROR] you must provide the parameter file's path"
+    exit 1
+fi
+
 terraform apply \
-    -var-file='params.tfvars.json' \
+    -var-file="$1" \
     -state=tf-data/rtf.tfstate
--- bin/plan.sh
+++ bin/plan.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+
+if [ "$#" -lt 1 ]; then 
+    echo "[ERROR] you must provide the parameter file's path"
+    exit 1
+fi
+
 terraform plan \
-    -var-file='params.tfvars.json' \
+    -var-file="$1" \
     -out=tf-data/rtf.plan
