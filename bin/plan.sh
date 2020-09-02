ae76e81973e80f95cd86835c3875177227a7236c
Souf <soufboulealf@gmail.com>
2020-09-02 18:30:25 +0200

    updates run scripts

--- bin/plan.sh
+++ bin/plan.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
-if [ "$#" -lt 2 ]; then 
+if [ "$#" -lt 1 ]; then 
     echo "[ERROR] you must provide the parameter file's path"
     exit 1
 fi
 
-terraform plan \
-    -var-file="$1" \
-    -target="$2" \
-    -out=tf-data/rtf.plan
+if [ "$#" -eq 1 ]; then
+    terraform plan \
+        -var-file="$1" \
+        -out=tf-data/rtf.plan
+fi
+
+if [ "$#" -eq 2 ]; then
+    terraform plan \
+        -var-file="$1" \
+        -target="$2" \
+        -out=tf-data/rtf.plan
+fi
+
+
