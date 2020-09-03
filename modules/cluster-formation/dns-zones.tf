df3a304f049015245db6b140c8b9d18f6fa6499d
Souf <soufboulealf@gmail.com>
2020-09-03 20:36:40 +0200

    uncomment sections

--- modules/cluster-formation/dns-zones.tf
+++ modules/cluster-formation/dns-zones.tf
@@ -1,5 +1,5 @@
 # amazonaws.com Zone 
-/* resource "alicloud_pvtz_zone" "amazonaws_pz" {
+resource "alicloud_pvtz_zone" "amazonaws_pz" {
   name  = "amazonaws.com"
   count = var.proxy_private_ip == "" ? 0 : 1
 }
@@ -10,10 +10,10 @@ resource "alicloud_pvtz_zone_record" "amazonaws" {
   type            = "A"
   value           = var.proxy_private_ip
   ttl             = 60
-} */
+}
 resource "alicloud_pvtz_zone_attachment" "amazonaws_pza" {
   count   = var.proxy_private_ip == "" ? 0 : 1
-  zone_id = "6bbdb7099f643efa9de36a84c6e88d3e" //alicloud_pvtz_zone.amazonaws_pz[0].id
+  zone_id = alicloud_pvtz_zone.amazonaws_pz[0].id
   vpc_ids = [alicloud_vpc.vpc[0].id]
 }
 
@@ -21,7 +21,7 @@ resource "alicloud_pvtz_zone_attachment" "amazonaws_pza" {
 #----------------------------------------------------------------------
 
 # PRIVATE ZONE
-/* resource "alicloud_pvtz_zone" "googleapis_pz" {
+resource "alicloud_pvtz_zone" "googleapis_pz" {
   name  = "googleapis.com"
   count = var.proxy_private_ip == "" ? 0 : 1
 }
@@ -32,10 +32,10 @@ resource "alicloud_pvtz_zone_record" "googleapis" {
   type            = "A"
   value           = var.proxy_private_ip
   ttl             = 60
-} */
+}
 resource "alicloud_pvtz_zone_attachment" "googleapis_pza" {
   count   = var.proxy_private_ip == "" ? 0 : 1
-  zone_id = "303be43aa603cb141c3f707b6df4442d" //alicloud_pvtz_zone.googleapis_pz[0].id
+  zone_id = alicloud_pvtz_zone.googleapis_pz[0].id
   vpc_ids = [alicloud_vpc.vpc[0].id]
 }
 
@@ -43,7 +43,7 @@ resource "alicloud_pvtz_zone_attachment" "googleapis_pza" {
 #----------------------------------------------------------------------
 
 # PRIVATE ZONE
-/* resource "alicloud_pvtz_zone" "msap_pz" {
+resource "alicloud_pvtz_zone" "msap_pz" {
   count = var.proxy_private_ip == "" ? 0 : 1
   name  = "msap.io"
 }
@@ -54,10 +54,10 @@ resource "alicloud_pvtz_zone_record" "msap" {
   type            = "A"
   value           = var.proxy_private_ip
   ttl             = 60
-} */
+}
 resource "alicloud_pvtz_zone_attachment" "msap_pza" {
   count   = var.proxy_private_ip == "" ? 0 : 1
-  zone_id = "8b3e6175f6f36a36cb55d0161e512527" //alicloud_pvtz_zone.msap_pz[0].id
+  zone_id = alicloud_pvtz_zone.msap_pz[0].id
   vpc_ids = [alicloud_vpc.vpc[0].id]
 }
 
@@ -65,7 +65,7 @@ resource "alicloud_pvtz_zone_attachment" "msap_pza" {
 #----------------------------------------------------------------------
 
 # PRIVATE ZONE
-/* resource "alicloud_pvtz_zone" "cloudhub_pz" {
+resource "alicloud_pvtz_zone" "cloudhub_pz" {
   count = var.proxy_private_ip == "" ? 0 : 1
   name  = "cloudhub.io"
 }
@@ -76,10 +76,10 @@ resource "alicloud_pvtz_zone_record" "cloudhub" {
   type            = "A"
   value           = var.proxy_private_ip
   ttl             = 60
-} */
+}
 resource "alicloud_pvtz_zone_attachment" "cloudhub_pza" {
   count   = var.proxy_private_ip == "" ? 0 : 1
-  zone_id = "6189226e22a4a974e27121535abc87b3" //alicloud_pvtz_zone.cloudhub_pz[0].id
+  zone_id = alicloud_pvtz_zone.cloudhub_pz[0].id
   vpc_ids = [alicloud_vpc.vpc[0].id]
 }
 
@@ -87,7 +87,7 @@ resource "alicloud_pvtz_zone_attachment" "cloudhub_pza" {
 #----------------------------------------------------------------------
 
 # PRIVATE ZONE
-/* resource "alicloud_pvtz_zone" "mulesoft_pz" {
+resource "alicloud_pvtz_zone" "mulesoft_pz" {
   count = var.proxy_private_ip == "" ? 0 : 1
   name  = "mulesoft.com"
 }
@@ -98,10 +98,10 @@ resource "alicloud_pvtz_zone_record" "mulesoft" {
   type            = "A"
   value           = var.proxy_private_ip
   ttl             = 60
-} */
+}
 resource "alicloud_pvtz_zone_attachment" "mulesoft_pza" {
   count   = var.proxy_private_ip == "" ? 0 : 1
-  zone_id = "eab722f7ac27b0cefe26caa696112cc5" //alicloud_pvtz_zone.mulesoft_pz[0].id
+  zone_id = alicloud_pvtz_zone.mulesoft_pz[0].id
   vpc_ids = [alicloud_vpc.vpc[0].id]
 }
 
